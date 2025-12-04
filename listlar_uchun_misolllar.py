904ec577c4808d6276344dc4ce939b52c0dea359
javohir <uzoqovjavohir632@gmail.com>
2025-12-04 08:25:45 +0500

    davomi bor

---
+++ listlar_uchun_misolllar.py
@@ -0,0 +1,67 @@
+# def orta(a, b, c):
+#     print("O'rtacha qiymat:", (a + b + c) / 3)
+
+# a = float(input("1-sonni kiriting: "))
+# b = float(input("2-sonni kiriting: "))
+# c = float(input("3-sonni kiriting: "))
+
+# orta(a, b, c)
+
+
+
+# def kvadrad(a ):
+#     print("sonning kvadrati = ", a**2)
+# a = float(input(" a sonni kiriting: "))
+# kvadrad(a)
+
+
+# def kub(a):
+#     print("sonning kvadrati = ", a**3)
+# a = float(input(" a sonni kiriting: "))
+# kub(a)
+
+
+
+# def katta(a, b):
+#     if a > b:
+#         print("Katta son:", a)
+#     elif b > a:
+#         print("Katta son:", b)
+#     else:
+#         print("Ikkala son teng")
+
+# a = float(input("1-sonni kiriting: "))
+# b = float(input("2-sonni kiriting: "))
+
+# katta(a, b)
+
+
+# def farangeyt ( t ):
+#     harorat = (t * 9/5) + 32    
+#     print("farangeyt boyicha harorat: " , harorat)  
+# selsiy = float(input("haroratni kiriting:  "))
+# farangeyt(selsiy)
+
+# def jadval(n):
+#     for i in range(1, 10+1):
+#         print(n, "*", i, "=", n * i)
+
+# son = int(input("Sonni kiriting: "))
+# jadval(son)
+
+
+# def kvadrat(x):
+#     return x ** 2
+
+# son = float(input("Sonni kiriting: "))
+# natija = kvadrat(son)
+# print("Kvadrati:", natija)
+
+
+def yigindi( a, b):
+    c= a+ b 
+    return c 
+a= float(input("1-sonni kiriting:  "))
+b= float(input("2-sonni kiriting:  "))
+natija = yigindi(a, b)
+print("Yig'indi:", natija)
